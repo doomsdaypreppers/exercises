1bdbdeef8bc195317045b1a3736741592e7b9a8f
Oscar Serna <oserna@gmail.com>
2019-07-17 07:21:47 +1000

    ready to code

--- exercise3/oserna/src/main/java/CommonAncestor.java
+++ exercise3/oserna/src/main/java/CommonAncestor.java
@@ -1,6 +1,6 @@
 public class CommonAncestor {
 
-    String findCommmonAncestor(String[] commitHashes, String[][] parentHashes, String commitHash1, String commitHash2){
+    public static String findCommmonAncestor(String[] commitHashes, String[][] parentHashes, String commitHash1, String commitHash2){
         return null;
     }
 
@@ -9,6 +9,10 @@ public class CommonAncestor {
         String [] commits = new String [] {"G","F","E","D","C","B","A"};
         String [][] parents = new String [][] {{"F","D"},{"E"},{"B"},{"C"},{"B"},{"A"},{null}};
 
+        String commit1 = "G";
+        String commit2 = "F";
+
+        String commmonAncestor = findCommmonAncestor(commits, parents, commit1, commit2);
 
     }
 
